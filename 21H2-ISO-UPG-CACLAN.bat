10499bd2230ec05d73a09fac1bd1797e7553a04a
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2022-04-26 12:56:46 -0700

    Update 21H2-ISO-UPG-CACLAN.bat

--- 21H2-ISO-UPG-CACLAN.bat
+++ 21H2-ISO-UPG-CACLAN.bat
@@ -1,15 +1,4 @@
-REM This batch file will attempt to do in-place upgrade to Windows 10 version 21H2 from the ISO-Extracted source.
-REM This is for the 64-bit version, which should be most computers, if not all. 
-REM This batch file checks and verifies make sure the target system is 64-bit first before running.
-
 @echo off
-
-dir /s %windir%\Web\4K
-if not errorlevel 1 goto end
-
-dir /s %systemdrive%\Program Files (x86)\Windows Defender
-if not errorlevel 1 goto start
-
 :start
 \\centralaz\shares\Tech\Support_Services\21H2_SetupEXE_PS_Upgrade\W10_21H2_ISO\setup.exe /auto upgrade /showoobe None /DynamicUpdate disable
 
