b85201fbdc33f1c04670dc63171df71e41fae353
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2022-04-26 14:00:27 -0700

    Update 21H2-ISO-UPG-CACLAN.bat

--- 21H2-ISO-UPG-CACLAN.bat
+++ 21H2-ISO-UPG-CACLAN.bat
@@ -1,7 +1,5 @@
 REM This batch file will attempt to do an in-place upgrade to Windows 10 64-bit version 21H2 from an ISO-Extracted source.
 
-@echo off
-
 :start
 \\centralaz\shares\Tech\Support_Services\21H2_SetupEXE_PS_Upgrade\W10_21H2_ISO\setup.exe /migchoice upgrade /showoobe none /eula accept /DynamicUpdate disable /Telemetry Disable /quiet
 
