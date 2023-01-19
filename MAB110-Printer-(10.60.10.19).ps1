6f460d4d7530b4a263ab718f1ef4cf40cad2a982
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2023-01-19 12:06:51 -0700

    Update MAB110-Printer-(10.60.10.19).ps1

--- MAB110-Printer-(10.60.10.19).ps1
+++ MAB110-Printer-(10.60.10.19).ps1
@@ -1,9 +1,13 @@
+$MYPRINTERDRV = "HP Universal Printing PCL 6"
+$MYPRINTER = "HP PRINTER MAR-B110 Learning Center"
+$MYPRINTERIP = "10.60.10.19"
+
 Get-Printer | Where-Object { $_.Name -notmatch "PDF" } | Remove-Printer
 Start-Sleep -Seconds 5
-Add-PrinterPort -Name "10.60.10.19" -PrinterHostAddress "10.60.10.19"
+Add-PrinterPort -Name $MYPRINTERIP -PrinterHostAddress $MYPRINTERIP
 Start-Sleep -Seconds 5
-pnputil.exe /a "\\spcstuapps01\Software\Drivers\HP Universal Print Driver\pcl6-x64-7.0.1.24923\*.inf"
+Add-Printer -DriverName $MYPRINTERDRV -Name $MYPRINTER -PortName $MYPRINTERIP
 Start-Sleep -Seconds 5
-Add-PrinterDriver -Name "HP Universal Printing PCL 6" -InfPath "C:\Windows\System32\DriverStore\FileRepository\hpcu255u.inf_amd64_883dd40f467c5d42\hpcu255u.inf"
-Start-Sleep -Seconds 5
-Add-Printer -DriverName "HP Universal Printing PCL 6" -Name "PRINTER MAR-B110 Learning Center" -PortName "10.60.10.19"
+
+$PRINTERTMP = (Get-CimInstance -ClassName CIM_Printer | WHERE {$_.Name -eq $MYPRINTER}[0])
+$PRINTERTMP | Invoke-CimMethod -MethodName SetDefaultPrinter | Out-Null
