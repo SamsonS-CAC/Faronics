417d04f038d68ad39c8a40d066b808845bc0f2ff
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2023-01-19 12:07:31 -0700

    Update MAB101-Printer-(10.60.10.18).ps1

--- MAB101-Printer-(10.60.10.18).ps1
+++ MAB101-Printer-(10.60.10.18).ps1
@@ -1,5 +1,13 @@
+$MYPRINTERDRV = "HP Universal Printing PCL 6"
+$MYPRINTER = "HP PRINTER MAR-B101 Library"
+$MYPRINTERIP = "10.60.10.18"
+
 Get-Printer | Where-Object { $_.Name -notmatch "PDF" } | Remove-Printer
 Start-Sleep -Seconds 5
-Add-PrinterPort -Name "10.60.10.18" -PrinterHostAddress "10.60.10.18"
+Add-PrinterPort -Name $MYPRINTERIP -PrinterHostAddress $MYPRINTERIP
 Start-Sleep -Seconds 5
-Add-Printer -DriverName "HP Universal Printing PCL 6" -Name "HP PRINTER MAR-B101 Library" -PortName "10.60.10.18"
+Add-Printer -DriverName $MYPRINTERDRV -Name $MYPRINTER -PortName $MYPRINTERIP
+Start-Sleep -Seconds 5
+
+$PRINTERTMP = (Get-CimInstance -ClassName CIM_Printer | WHERE {$_.Name -eq $MYPRINTER}[0])
+$PRINTERTMP | Invoke-CimMethod -MethodName SetDefaultPrinter | Out-Null
