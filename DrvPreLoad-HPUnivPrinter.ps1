3376a2ae1f549308939f4a1626fabeac8dc59269
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2023-01-19 12:58:18 -0700

    Update DrvPreLoad-HPUnivPrinter.ps1

--- DrvPreLoad-HPUnivPrinter.ps1
+++ DrvPreLoad-HPUnivPrinter.ps1
@@ -1,3 +1,5 @@
 pnputil.exe /a "\\spcstuapps01\Software\Drivers\HP Universal Print Driver\pcl6-x64-7.0.1.24923\*.inf"
 Start-Sleep -Seconds 10
+
 Add-PrinterDriver -Name "HP Universal Printing PCL 6" -InfPath "C:\Windows\System32\DriverStore\FileRepository\hpcu255u.inf_amd64_883dd40f467c5d42\hpcu255u.inf"
+Start-Sleep -Seconds 1
