3540e0ad51ac53201e4e229baf069f51d6f9952d
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2023-01-19 12:06:09 -0700

    Update STC117-Printer-(10.70.10.14).ps1

--- STC117-Printer-(10.70.10.14).ps1
+++ STC117-Printer-(10.70.10.14).ps1
@@ -1,5 +1,13 @@
+$MYPRINTERDRV = "HP Universal Printing PCL 6"
+$MYPRINTER = "HP PRINTER STC-C117 Learning Center"
+$MYPRINTERIP = "10.70.10.14"
+
 Get-Printer | Where-Object { $_.Name -notmatch "PDF" } | Remove-Printer
 Start-Sleep -Seconds 5
-Add-PrinterPort -Name "10.70.10.14" -PrinterHostAddress "10.70.10.14"
+Add-PrinterPort -Name $MYPRINTERIP -PrinterHostAddress $MYPRINTERIP
 Start-Sleep -Seconds 5
-Add-Printer -DriverName "HP Universal Printing PCL 6" -Name "HP PRINTER STC-C117 Learning Center" -PortName "10.70.10.14"
+Add-Printer -DriverName $MYPRINTERDRV -Name $MYPRINTER -PortName $MYPRINTERIP
+Start-Sleep -Seconds 5
+
+$PRINTERTMP = (Get-CimInstance -ClassName CIM_Printer | WHERE {$_.Name -eq $MYPRINTER}[0])
+$PRINTERTMP | Invoke-CimMethod -MethodName SetDefaultPrinter | Out-Null
