29edef04084dc08f05cbc35daa760d2eabc85205
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2023-01-19 11:10:54 -0700

    Create MAB101-Printer-(10.60.10.18).ps1

---
+++ MAB101-Printer-(10.60.10.18).ps1
@@ -0,0 +1,9 @@
+Get-Printer | Where-Object { $_.Name -notmatch "PDF" } | Remove-Printer
+Start-Sleep -Seconds 5
+Add-PrinterPort -Name "10.60.10.18" -PrinterHostAddress "10.60.10.18"
+Start-Sleep -Seconds 5
+pnputil.exe /a "\\spcstuapps01\Software\Drivers\HP Universal Print Driver\pcl6-x64-7.0.1.24923\*.inf"
+Start-Sleep -Seconds 5
+Add-PrinterDriver -Name "HP Universal Printing PCL 6" -InfPath "C:\Windows\System32\DriverStore\FileRepository\hpcu255u.inf_amd64_883dd40f467c5d42\hpcu255u.inf"
+Start-Sleep -Seconds 5
+Add-Printer -DriverName "HP Universal Printing PCL 6" -Name "PRINTER MAR-B101 Library" -PortName "10.60.10.18"
