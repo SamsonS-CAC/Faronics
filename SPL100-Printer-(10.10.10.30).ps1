922bf9970f64dedaf60c1581a3fed95b334d3c08
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2023-01-23 12:29:56 -0700

    Create SPL100-Printer-(10.10.10.30).ps1

---
+++ SPL100-Printer-(10.10.10.30).ps1
@@ -0,0 +1,18 @@
+$MYPRINTERDRV = "HP Universal Printing PCL 6"
+$MYPRINTER = "HP PRINTER SPC-L100 Library"
+$MYPRINTERIP = "10.10.10.30"
+
+Get-Printer | Where-Object { $_.Name -notmatch "PDF" } | Remove-Printer
+Start-Sleep -Seconds 5
+Add-PrinterPort -Name $MYPRINTERIP -PrinterHostAddress $MYPRINTERIP
+Start-Sleep -Seconds 5
+Add-Printer -DriverName $MYPRINTERDRV -Name $MYPRINTER -PortName $MYPRINTERIP
+Start-Sleep -Seconds 5
+
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" -Name "LegacyDefaultPrinterMode" -Value 1 -Force
+Start-Sleep -Seconds 4
+
+$PRINTERTMP = (Get-CimInstance -ClassName CIM_Printer | WHERE {$_.Name -eq $MYPRINTER}[0])
+$PRINTERTMP | Invoke-CimMethod -MethodName SetDefaultPrinter | Out-Null
+Start-Sleep -Seconds 1
+
