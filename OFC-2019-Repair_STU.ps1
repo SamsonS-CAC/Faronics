8382d48d48502dac28f0b47d21d5f9fdaed871bc
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2024-09-19 15:49:23 -0700

    Create OFC-2019-Repair_STU.ps1

---
+++ OFC-2019-Repair_STU.ps1
@@ -0,0 +1,253 @@
+#requires -version 2
+#//Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+  This script downloads the current Office uninstaller from Microsoft and tries to remove all Office installations on this computer.
+  By choosing -InstallOffice2019 it tries to install Office 2019 as well.
+.DESCRIPTION
+  By choosing -InstallOffice2019 it tries to install Office 2019 as well.
+.PARAMETER InstallOffice2019
+  Will install Office2019 after removal.
+.PARAMETER SuppressReboot
+  Will supress the reboot after finishing the script.
+.PARAMETER UseSetupRemoval
+  Will use the setup method to remove current Office installations instead of SaRA.
+.PARAMETER Force
+  Skip user-input.
+.PARAMETER RunAgain
+  Skip Stage validation and runs the whole script again.
+.PARAMETER SecondsToReboot
+  Seconds before the computer will reboot.
+.INPUTS
+  None
+.OUTPUTS
+  Just output on screen
+.NOTES
+  Version:        1.2
+.EXAMPLE
+  .\MSOffice-Remove.ps1 -SupressReboot -Force
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+Param (
+    [switch]$InstallOffice2019 = $True,
+    [switch]$SuppressReboot = $True,
+    [switch]$UseSetupRemoval = $True,
+    [Switch]$Force = $True,
+    [switch]$RunAgain = $True,
+    [int]$SecondsToReboot = 60
+)
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+$SaRA_URL = "https://aka.ms/SaRA_CommandLineVersionFiles"
+$SaRA_ZIP = "$env:TEMP\SaRA.zip"
+$SaRA_DIR = "$env:TEMP\SaRA"
+$SaRA_EXE = "$SaRA_DIR\SaRAcmd.exe"
+$Office2019Setup_URL = "\\10.10.2.71\Software\_IMAGING_\Apps\Office2019"
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+Function Invoke-OfficeUninstall {
+    if (-Not (Test-Path "$SaRA_DIR")) {
+        New-Item "$SaRA_DIR" -ItemType Directory | Out-Null
+    }
+    if ($UseSetupRemoval) {
+        Write-Host "Invoking default setup method ..."
+        Invoke-SetupOffice2019 "$Office2019Setup_URL/purge.xml"
+    }
+    else {
+        Write-Host "Invoking SaRA method ..."
+        Remove-SaRA
+        Write-Host "Downloading most recent SaRA build ..."
+        Invoke-SaRADownload
+        Write-Host "Removing Office installations ..."
+        Invoke-SaRA 
+    }
+}
+Function Invoke-SaRADownload {    
+    Start-BitsTransfer -Source "$SaRA_URL" -Destination "$SaRA_ZIP" 
+    if (Test-Path "$SaRA_ZIP") {
+        Write-Host "Unzipping ..."
+        Expand-Archive -Path "$SaRA_ZIP" -DestinationPath "$SaRA_DIR" -Force
+        if (Test-Path "$SaRA_DIR\DONE") {
+            Move-Item "$SaRA_DIR\DONE\*" "$SaRA_DIR" -Force
+            if (Test-Path "$SaRA_EXE") {
+                Write-Host "SaRA downloaded successfully."
+            }
+            else {
+                Write-Error "Download failed. Program terminated."
+                Exit 1
+            }
+        }
+    }
+}
+
+Function Remove-SaRA {  
+    if (Test-Path "$SaRA_ZIP") {
+        Remove-Item "$SaRA_ZIP" -Force
+    }
+
+    if (Test-Path "$SaRA_DIR") {
+        Remove-Item "$SaRA_DIR" -Recurse -Force
+    }
+}
+ 
+Function Stop-OfficeProcess {
+    Write-Host "Stopping running Office applications ..."
+    $OfficeProcessesArray = "lync", "winword", "excel", "msaccess", "mstore", "infopath", "setlang", "msouc", "ois", "onenote", "outlook", "powerpnt", "mspub", "groove", "visio", "winproj", "graph", "teams"
+    foreach ($ProcessName in $OfficeProcessesArray) {
+        if (get-process -Name $ProcessName -ErrorAction SilentlyContinue) {
+            if (Stop-Process -Name $ProcessName -Force -ErrorAction SilentlyContinue) {
+                Write-Output "Process $ProcessName was stopped."
+            }
+            else {
+                Write-Warning "Process $ProcessName could not be stopped."
+            }
+        } 
+    }
+}
+
+Function Invoke-SaRA {
+    $SaRAProcess = Start-Process -FilePath "$SaRA_EXE" -ArgumentList "-S OfficeScrubScenario -AcceptEula -CloseOffice -OfficeVersion All" -Wait -PassThru -NoNewWindow
+    switch ($SaRAProcess.ExitCode) {
+        0 {
+            Write-Host "Uninstall successful!"
+            Set-CurrentStage 2
+            Break
+        }
+    
+        7 {
+            Write-Host "No office installations found."
+            Set-CurrentStage 2
+            Break
+        }
+
+        8 {
+            Write-Error "Multiple office installations found. Program need to be run in GUI mode."
+            Set-CurrentStage 4
+            Exit 2
+        }
+
+        9 {
+            Write-Error "Uninstall failed! Program need to be run in GUI mode."
+            Set-CurrentStage 4
+            Exit 3
+        }
+    }
+}
+
+Function Invoke-SetupOffice2019($Office2019ConfigFile) {
+    if ($Office2019ConfigFile -eq "$Office2019Setup_URL/purge.xml") {
+        Write-Host "Downloading Office2019 Installer ..."
+        Start-BitsTransfer -Source "$Office2019Setup_URL/setup.exe" -Destination "$SaRA_DIR\setup.exe"
+        Start-BitsTransfer -Source "$Office2019ConfigFile" -Destination "$SaRA_DIR\purge.xml"
+        Write-Host "Executing Office2019 Setup ..."
+        $OfficeSetup = Start-Process -FilePath "$SaRA_DIR\setup.exe" -ArgumentList "/configure $SaRA_DIR\purge.xml" -Wait -PassThru -NoNewWindow 
+    }
+    
+    if ($InstallOffice2019) {
+        Write-Host "Downloading Office2019 Installer ..."
+        Start-BitsTransfer -Source "$Office2019Setup_URL/setup.exe" -Destination "$SaRA_DIR\setup.exe"
+        Start-BitsTransfer -Source "$Office2019ConfigFile" -Destination "$SaRA_DIR\config.xml"
+        Write-Host "Executing Office2019 Setup ..."
+        $OfficeSetup = Start-Process -FilePath "$SaRA_DIR\setup.exe" -ArgumentList "/configure $SaRA_DIR\config.xml" -Wait -PassThru -NoNewWindow 
+        switch ($OfficeSetup.ExitCode) {
+            0 {
+                Write-Host "Install successful!"
+                Set-CurrentStage 4
+                Break
+            }
+
+            1 {
+                Write-Error "Install failed!"
+                Set-CurrentStage 3
+                Break
+            }
+        }
+    }
+}
+
+Function Invoke-RebootInSeconds($Seconds) {
+    if (-not $SuppressReboot) {
+        Start-Process -FilePath "$env:SystemRoot\system32\shutdown.exe" -ArgumentList "/r /c `"Reboot needed. System will reboot in $Seconds seconds.`" /t $Seconds /f /d p:4:1"
+    }
+}
+
+Function Set-CurrentStage($StageValue) {
+    if (-not (Test-Path "HKLM:\Software\OEM\CAC-ITSA\O2019\Install")) {
+        New-Item -Path "HKLM:\Software\OEM\CAC-ITSA\O2019\Install" -Force | Out-Null
+    }
+    New-ItemProperty -Path "HKLM:\Software\OEM\CAC-ITSA\O2019\Install" -Name "CurrentStage" -Value $StageValue -PropertyType String -Force | Out-Null
+}
+
+Function Invoke-Intro {   
+Write-Host "Microsoft Office Removal Tool"
+Write-Host ""
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+# Check if -Force is set
+if (-Not $Force) {
+    do {
+        $YesOrNo = Read-Host "Are you sure you want to remove Office from this PC? (y/n)"
+    } while ("y", "n" -notcontains $YesOrNo)
+
+    if ($YesOrNo -eq "n") {
+        exit 1
+    }
+}
+
+Invoke-Intro
+# Check if there is a stage to resume
+if (-not ($RunAgain)) {
+    if (Test-Path "HKLM:\Software\OEM\CAC-ITSA\O2019\Install") {
+        $CurrentStageValue = (Get-ItemProperty "HKLM:\Software\OEM\CAC-ITSA\O2019\Install").CurrentStage
+        Switch ($CurrentStageValue) {
+            1 {
+                Write-Host "Resuming Stage 1: Uninstalling Office ..."
+                Invoke-OfficeUninstall 
+                Invoke-SetupOffice2019 "$Office2019Setup_URL/upgrade.xml"
+                Remove-SaRA
+                Invoke-RebootInSeconds $SecondsToReboot
+            }
+
+            2 {
+                Write-Host "Resuming Stage 2: Installing Office 2019 ..."
+                Invoke-SetupOffice2019 "$Office2019Setup_URL/upgrade.xml"
+                Remove-SaRA
+                Invoke-RebootInSeconds $SecondsToReboot
+            }
+
+            3 {
+                Write-Host "Resuming Stage 3: Cleaning up ..."
+                Remove-SaRA
+            }
+
+            4 {
+                # Final stage: All is done, script will not run.
+                exit 0
+            }
+
+            default {
+                Write-Host "Resuming Stage 1: Uninstalling Office ..."
+                Invoke-OfficeUninstall 
+                Invoke-SetupOffice2019 "$Office2019Setup_URL/upgrade.xml"
+                Remove-SaRA
+                Invoke-RebootInSeconds $SecondsToReboot
+            }
+        }
+    }
+    else {
+        Invoke-Intro
+        Stop-OfficeProcess
+        Invoke-OfficeUninstall 
+        Invoke-SetupOffice2019 "$Office2019Setup_URL/upgrade.xml"
+        Invoke-RebootInSeconds $SecondsToReboot
+    }
+}
+else {
+    Invoke-Intro
+    Stop-OfficeProcess
+    Invoke-OfficeUninstall 
+    Invoke-SetupOffice2019 "$Office2019Setup_URL/upgrade.xml"
+    Invoke-RebootInSeconds $SecondsToReboot
+}
+exit
