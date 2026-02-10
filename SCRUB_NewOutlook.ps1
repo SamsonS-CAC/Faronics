825ec214798575308af1e839a30db20c82050a0b
SamsonS-CAC <104104109+SamsonS-CAC@users.noreply.github.com>
2026-02-09 17:27:15 -0700

    Update SCRUB_NewOutlook.ps1

--- SCRUB_NewOutlook.ps1
+++ SCRUB_NewOutlook.ps1
@@ -1,1 +1,5 @@
 Remove-AppxProvisionedPackage -AllUsers -Online -PackageName (Get-AppxPackage Microsoft.OutlookForWindows).PackageFullName
+Remove-AppxPackage -AllUsers -Package (Get-AppxPackage Microsoft.OutlookForWindows).PackageFullName
+
+Remove-AppxProvisionedPackage -AllUsers -Online -PackageName (Get-AppxPackage microsoft.windowscommunicationsapps).PackageFullName
+Remove-AppxPackage -AllUsers -Package (Get-AppxPackage microsoft.windowscommunicationsapps).PackageFullName
